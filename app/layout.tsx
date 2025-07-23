72a771fc078a26a8bb5208c8817d9da0d75c10aa
Sarah Paluszny <sarah.paluszny@gmail.com>
2025-07-23 11:52:13 -0400

    update: icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import { Inria_Sans, Inria_Serif } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/Components/navbar";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 const inriaSans = Inria_Sans({
   variable: "--font-inria-sans",
@@ -38,7 +29,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} ${inriaSans.variable} ${inriaSerif.variable} antialiased`}>
+      <body className={` ${inriaSans.variable} ${inriaSerif.variable} antialiased`}>
         <Navbar />
         <main>
           {children}
---
+++ config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+    name: "Sarah Paluszny Frontend Engineer Portfolio",
+    url: "https://test.com",
+    description: "Portfolio site showing all of Sarah Paluszny's Frontend Engineering Projects",
+    author: "Sarah Paluszny",
+    links: {
+        github: "https://github.com/spaluszny",
+        linkedIn: "https://www.linkedin.com/in/sarah-paluszny/",
+        leetCode: "https://leetcode.com/u/spaluszny/"
+    },
+};
+
+export type SiteConfig = typeof siteConfig
